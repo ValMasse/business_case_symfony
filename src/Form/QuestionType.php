59ea8c04412c760f97a31e52bd4812e15867b95c
ValMasse <valmasse@gmx.fr>
2020-09-04 16:46:18 +0200

    crud for Question

---
+++ src/Form/QuestionType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Question;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('position')
+            ->add('enonce')
+            ->add('estEliminatoire')
+            ->add('estActif')
+            ->add('domaine')
+            ->add('niveau')
+            ->add('testsTechniques')
+            ->add('administrateurs')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Question::class,
+        ]);
+    }
+}
