acdb01d0b954909d73fcd997fd0fd90e83e2eaaa
ValMasse <valmasse@gmx.fr>
2020-10-03 00:46:49 +0200

    work on display each question of test need to  paginate

--- src/Controller/PasserTestController.php
+++ src/Controller/PasserTestController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
 
 class PasserTestController extends AbstractController
 {
@@ -30,6 +31,22 @@ class PasserTestController extends AbstractController
         return $this->render('passer_test/start.html.twig', [
             'tests_techniques' => $testTechniqueRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_USER")
+     * @Route("/{id}", name="test_technique_show", methods={"GET","POST"})
+     */
+    public function testStart(Request $request, TestTechnique $testTechnique): Response
+    {
+       $id = $request->attributes->get('_route_params');
+       $testTechniqueId = $id['id'];
+       //var_dump($testTechniqueId);
+       
+        return $this->render('test_technique/show.html.twig', [
+            'test_technique' => $testTechnique,
+            'questions' => $questionRepository->findQuestionsForEachTest($testTechniqueId),
+            ]);
     }
 
 }
